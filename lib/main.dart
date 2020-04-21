c39087375d79cc4362cae60f8ed8dcd828df7f16
Prasanna Anbazhagan <praslnx8@gmail.com>
2020-04-21 08:43:32 +0530

    Add firebase login integration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:movie_suggestion/screens/login_screen.dart';
 
-void main() => runApp(MyApp());
+final HttpLink httpLink = HttpLink(
+  uri: 'https://api.github.com/graphql',
+);
+
+final AuthLink authLink = AuthLink(
+  getToken: () async => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
+  // OR
+  // getToken: () => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
+);
+
+final Link link = authLink.concat(httpLink);
+
+ValueNotifier<GraphQLClient> client = ValueNotifier(
+  GraphQLClient(
+    cache: InMemoryCache(),
+    link: link,
+  ),
+);
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Watch Next',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginScreen(),
-    );
+    return GraphQLProvider(
+        client: client,
+        child: CacheProvider(
+            child: MaterialApp(
+          title: 'Watch Next',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: LoginScreen(),
+        )));
   }
 }
